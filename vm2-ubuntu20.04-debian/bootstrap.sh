975e480cdc7d052544ffa021e8601c9c7dedb3c7
Alex Berber <berber.it@gmail.com>
2021-11-08 16:31:12 +0200

    first commit

---
+++ vm2-ubuntu20.04-debian/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo "Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "Set root password"
+echo -e "admin\nadmin" | passwd root >/dev/null 2>&1
+
+# Install docker 
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+systemctl start docker
